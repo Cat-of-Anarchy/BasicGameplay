223e70b3101c35984f41be9f0ab33ddc59c2fdab
Cat of Anarchy <greffej@roverkids.org>
2024-12-09 13:32:34 -0500

    fished 2.4

--- Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -16,6 +16,16 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z > topbound) { Destroy(gameObject); } else if (transform.position.z < lowerBound) { Destroy(gameObject); }
+        if (transform.position.z > topbound)
+
+        { Destroy(gameObject); }
+
+        else if (transform.position.z < lowerBound)
+
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject); 
+           }
+
     }
 }
